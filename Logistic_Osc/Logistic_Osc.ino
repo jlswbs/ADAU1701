69615227d89ed3deba1c781536534910eb867097
jlswbs <jlswbs@users.noreply.github.com>
2019-11-17 17:49:30 +0100

    Add files via upload

---
+++ Logistic_Osc/Logistic_Osc.ino
@@ -0,0 +1,20 @@
+// Logistic Equation //
+
+#include <SigmaDSP.h>
+#include "SigmaDSP_parameters.h"
+
+SigmaDSP dsp(DSP_I2C_ADDRESS, ADAU1701 /*,12*/);
+
+void setup() 
+{  
+ 
+  dsp.begin(/* SDA, SCL */);
+  loadProgram(dsp);
+
+}
+
+
+void loop() 
+{
+ 
+}

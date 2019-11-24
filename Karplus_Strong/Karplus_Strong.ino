4bf05a6b3065d2b26e06c968e4c75afc23803d36
jlswbs <jlswbs@users.noreply.github.com>
2019-11-24 20:37:10 +0100

    Add files via upload

---
+++ Karplus_Strong/Karplus_Strong.ino
@@ -0,0 +1,20 @@
+// Karplus Strong //
+
+#include <SigmaDSP.h>
+#include "SigmaDSP_parameters.h"
+
+SigmaDSP dsp(DSP_I2C_ADDRESS, ADAU1701 /*,12*/);
+
+void setup() 
+{  
+ 
+  dsp.begin(/* SDA, SCL */);
+  loadProgram(dsp);
+
+}
+
+
+void loop() 
+{
+ 
+}
